3a5a36b75c18e87bb0ed3af81739784b57272af7
VaniThapar <99047323+VaniThapar@users.noreply.github.com>
2023-07-12 13:20:51 +0530

    Update users.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,11 +2,7 @@ import bcrypt from "bcryptjs";
 
 const users = [
   {
-    name: "Rajat",
-    email: "rajatthapar1996@gmail.com",
-    password: bcrypt.hashSync("taraa@2127", 10),
-    isAdmin: true,
-  },
+    
   {
     name: "John Doe",
     email: "john@gmail.com",
